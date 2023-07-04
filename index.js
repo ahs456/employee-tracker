35790660edeea66db64489713136f9d215e976a1
Axh <anikahussain1@outlook.com>
2023-07-04 16:20:10 +0100

    connected to database

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
-const { MainQuestions, AddDepartmentQuestion, AddEmployeeQuestion, AddRoleQuestion, UpdateEmployeeRoleQuestion} = require('questions.js');
-const EmployeeDatabase  = require('db/EmployeeDatabase.js');
-const { connect } = require('http2');
+const { MainQuestions, AddDepartmentQuestion, AddEmployeeQuestion, AddRoleQuestion, UpdateEmployeeRoleQuestion} = require('./questions.js');
+const EmployeeDatabase  = require('./db/EmployeeDatabase.js');
 
 const db = new EmployeeDatabase({
     host: 'localhost',
@@ -15,5 +14,29 @@ db.connect();
 const doFirstQuestions = () => {
     inquirer
     .prompt(MainQuestions)
-    .then((respone))
+    .then((respone) => {
+        switch (respone.option) {
+            case 'view_departments':
+                view_departments();
+                break;
+            case 'view_roles':
+                view_roles();
+                break;
+            case 'view_employees':
+                view_employees();
+                break;
+            case 'add_department':
+                add_department();
+                break;
+            case 'add_role':
+                add_role();
+                break;
+            case 'add_employee':
+                add_employee();
+                break;
+            case 'uopdate_role':
+                update_role();
+                break;
+        }
+    })
 }
